8d042ec5b24c4f2ff160b4095f296c3484e9c3ec
lkotlarenko <lkotlarenko@protonmail.com>
2022-02-01 10:48:08 -0300

    Req 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  beforeEach(() => { 
+    const { history } = renderWithRouter(<App />);
+    history.push('/duvidoqueexisteessapagina');
+  });
+  test('Teste se página contém um h2 com o texto "Page requested not found 😭"', () => {
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const imageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(image).toHaveProperty('src', imageSrc);
+  });
+});
